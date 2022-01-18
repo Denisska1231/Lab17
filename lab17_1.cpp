741d1940b76cd917b44c282fb6d9c05b438a079f
Denisska1231 <jedthaphon@gmail.com>
2022-01-18 17:06:26 +0700

    Completed lab17_1

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -10,7 +10,9 @@ void randData(double *,int,int);
 
 void findRowSum(const double *,double *,int,int);
 
-void findColSum(const double *,double *,int,int);
+void findColSum(const double *,double *,int,int){
+
+}
 
 int main(){
 	srand(time(0));
@@ -31,4 +33,43 @@ int main(){
 	
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
+}
+
+void showData(double *a,int N,int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << *(a+i);
+        if((i+1)%M==0) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double *a,int N,int M){
+    for(int i = 0; i < N*M; i++){
+        *(a+i) = (rand()%101)/100.0;
+    }
+}
+
+void findRowSum(const double *a,double *b,int N,int M){
+    int x=0;
+    double sum=0;
+    for(int i = 0; i < N*M; i++){
+        sum += *(a+i);
+        if((i+1)%M==0){
+        b[x]=sum;
+        sum=0;
+        x += 1;
+        }
+        else{
+        }
+    }
+}
+
+void findColSum(const double *a,double *b,int N,int M){
+    for(int i=0; i<M; i++){
+        b[i]=0;
+        for(int j=0; j<N; j++){
+            *(b+i) += *(a+i+M*j);
+        }
+    }
 }
